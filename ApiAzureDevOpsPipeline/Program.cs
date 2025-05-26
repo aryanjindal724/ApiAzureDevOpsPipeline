10603700d620dedf2f11310011c6de82665351d1
Aryan Jindal <aryanjindal455@gmail.com>
2025-05-26 11:52:19 +0530

    updated code

--- ApiAzureDevOpsPipeline/Program.cs
+++ ApiAzureDevOpsPipeline/Program.cs
@@ -19,9 +19,11 @@ namespace ApiAzureDevOpsPipeline
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
